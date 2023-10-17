eb0f7ae166f5df0472e80780e94feb583f83e7f3
Alumnos <Alumnos@PC188>
2023-10-17 14:18:35 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace pryBDSocios
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEstadoConexion = new System.Windows.Forms.Label();
+            this.lblDatos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEstadoConexion
+            // 
+            this.lblEstadoConexion.AutoSize = true;
+            this.lblEstadoConexion.Location = new System.Drawing.Point(26, 21);
+            this.lblEstadoConexion.Name = "lblEstadoConexion";
+            this.lblEstadoConexion.Size = new System.Drawing.Size(35, 13);
+            this.lblEstadoConexion.TabIndex = 0;
+            this.lblEstadoConexion.Text = "label1";
+            // 
+            // lblDatos
+            // 
+            this.lblDatos.AutoSize = true;
+            this.lblDatos.Location = new System.Drawing.Point(382, 142);
+            this.lblDatos.Name = "lblDatos";
+            this.lblDatos.Size = new System.Drawing.Size(35, 13);
+            this.lblDatos.TabIndex = 1;
+            this.lblDatos.Text = "label1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDatos);
+            this.Controls.Add(this.lblEstadoConexion);
+            this.Name = "frmMain";
+            this.Text = "EL CLUB";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEstadoConexion;
+        private System.Windows.Forms.Label lblDatos;
+    }
+}
+
---
+++ clsAccesoDatos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace pryBDSocios
+{
+    internal class clsAccesoDatos
+    {
+        OleDbConnection conexionBD;
+        OleDbCommand comandoBD; //indica que quiero traer de las tablas
+        OleDbDataReader lectorBD; //carga info para desp leer
+
+        string cadenaConexion = @"Provider = Microsoft.ACE.OLEDB.12.0;" + " Data Source = ..\\..\\Resources\\EL_CLUB.accdb";
+        
+        public string estadoConexion = "";
+
+        public string datosTabla = "";
+
+        public void ConectarBD(){
+            try
+            {
+                conexionBD = new OleDbConnection();
+                conexionBD.ConnectionString = cadenaConexion;
+                conexionBD.Open();
+                estadoConexion = "Conectado";
+            }
+            catch (Exception ex)
+            {
+
+                estadoConexion ="Error: "+ ex.Message;
+            }
+            
+        }
+
+        public void TraerDatos()
+        {
+            comandoBD = new OleDbCommand();
+
+            comandoBD.Connection = conexionBD;
+            comandoBD.CommandType = System.Data.CommandType.TableDirect;  //q tipo de operacion quierp hacer y que me traiga TOD la tabla con el tabledirect
+            comandoBD.CommandText = "SOCIOS"; //Que tabla traigo
+
+           lectorBD = comandoBD.ExecuteReader(); //
+
+            if (lectorBD.HasRows) //SI TIENE FILAS
+            {
+                while (lectorBD.Read())
+                {
+                    datosTabla += "-" +lectorBD[0]; //dato d la comlumna 0
+                }
+
+            }
+
+
+        }
+    }
+}   
